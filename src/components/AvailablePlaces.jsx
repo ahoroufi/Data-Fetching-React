e86ce44b206bca466d8c85697871a727bdf32578
Arash Horoufi <6829077+ahoroufi@users.noreply.github.com>
2026-01-30 17:55:16 -0800

    233. Preparing the App for data fatching

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,15 @@
+import Places from './Places.jsx';
+import { useState } from 'react';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  return (
+    <Places
+      title="Available Places"
+      places={[]}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
